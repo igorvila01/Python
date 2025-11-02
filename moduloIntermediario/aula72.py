25ea7ce937780ff51b8972010a6f5b5cfdfd5d2c
igor <igor.vila01@gmail.com>
2025-11-02 12:03:23 -0300

    correcao_professor

--- moduloIntermediario/aula72.py
+++ moduloIntermediario/aula72.py
@@ -5,7 +5,7 @@ Crie uma função que mutiplica todos os argumentos
 não nomeados recebidos
 Retorne o total para uma variavel e mostre o valor da variavel
 """
-print(5*6*7*8*9*10)
+
 
 def multiplica(*args):
     total = 1   
@@ -25,14 +25,12 @@ Retorne se um numero é par ou impar.
 
 
 def par_impar(x):
-    if x % 2 == 0:
-        resultado = "Numero Par"
-        return resultado
-    else:
-        resultado = 'Numero Impar'
-        return resultado
+    if x % 2 == 0:        
+        return f'{x} é Par'
+    
+    return f'{x} é Impar'
 
 
-print(par_impar(1))
+print(par_impar(2))
 
 
