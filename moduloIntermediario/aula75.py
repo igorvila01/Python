4194918d2316437b759c1ae9a020098d4a1c0b5f
Igor Vila <103008816+igorvila01@users.noreply.github.com>
2025-11-04 13:21:55 -0300

    Implement multiplication function for given number
    Added a function to perform multiplication by 2, 3, and 4.

--- moduloIntermediario/aula75.py
+++ moduloIntermediario/aula75.py
@@ -3,4 +3,9 @@ Exercicios
 Crie funçoes que duplicam, triplicam e quadruplicam 
 o numero recebido como parametro 
 """
+def operacao(num):
+    print(num)
+    for numero in range(2,5):
+        print(f'{num} x {numero} = {num*numero}')
 
+operacao(2)
