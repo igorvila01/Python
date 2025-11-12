c792239e2f854939261882a2d6ff759ea704a56e
igor <igor.vila01@gmail.com>
2025-11-11 22:01:46 -0300

    enunciado_exercico_dicionarios

---
+++ moduloIntermediario/aula77.py
@@ -0,0 +1,21 @@
+"""
+Exercicio de sistema de perguntas e respostas 
+"""
+
+perguntas = [
+    {
+        'pergunta': 'Quanto é 2+2?',
+        'opcoes': ['1', '3', '4', '5'],
+        'resposta': '4'
+    },
+    {
+        'pergunta': 'Quanto é 5*5?',
+        'opcoes': ['25', '55', '10', '51'],
+        'resposta': '25'
+    },
+    {
+        'pergunta': 'Quanto é 10/2?',
+        'opcoes': ['4', '5', '2', '1'],
+        'resposta': '5'
+    }
+]
