501b28a05b8f88027fec81d577cc37e2b621d2a8
igor <igor.vila01@gmail.com>
2026-04-25 14:06:26 -0300

    aula 122

---
+++ POO/aula122.py
@@ -0,0 +1,17 @@
+# Escopo da classe e de metodos da classe
+class Animal:
+    def __init__(self, nome):
+        self.nome = nome
+        
+    def comendo(self, alimento):
+        return f'O {self.nome} esta comendo {alimento}'
+    
+    def executar(self, *args, **kwargs):
+        return self.comendo(*args, **kwargs)
+    
+    
+cachorro = Animal('Cachorro')
+print(cachorro.executar('Ração'))
+
+
+
