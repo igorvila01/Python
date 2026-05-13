7e4c157e01db4925e509a1b4d93bf5cd962a6562
igor <igor.vila01@gmail.com>
2026-05-12 21:31:31 -0300

    aula sobre json

---
+++ moduloIntermediario/module.py
@@ -0,0 +1,8 @@
+Pessoa = [
+    {"nome": "Igor",
+     "sobrenome": "Vila",
+     "idade": 30, 
+     "ativo": True,
+     "notas": [10, 9, 7],
+     "telefones": {"residencial": "(00) 00000-0000"}}
+]
